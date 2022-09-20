99660ee95e4474d552ef4d3a188599b6117d815b
Posya0 <posya.prozorova.02@gmail.com>
2022-09-20 19:46:45 +0300

    Исправлено 2

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
-def lcs_length():
+def lcs_length(s, t):
     from collections import Counter
-
-    s = input('Введите 1 строку:')
-    t = input('Введите 2 строку:')
-
     dp = Counter()
 
     for i in range(len(s)):
         for j in range(len(t)):
             if s[i] == t[j]:
-                dp[i, j] = dp[i - 1, j] + 1
+                dp[i, j] = dp[i - 1, j - 1] + 1
             else:
                 dp[i, j] = max(dp[i, j - 1], dp[i - 1, j])
     return max(dp.values()) if dp else 1
 
 
-print(lcs_length())
+print(lcs_length("weow", "homeone"))
